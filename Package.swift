a75f634b6211470e750eb2219e72f551726e0152
Chris Lunsford <chris@cmlccie.com>
2019-02-25 22:58:46 -0500

    Rename Library

--- Package.swift
+++ Package.swift
@@ -4,25 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-algorithms",
+    name: "SwiftAlgorithms",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "swift-algorithms",
-            targets: ["swift-algorithms"]),
+            name: "SwiftAlgorithms",
+            targets: ["SwiftAlgorithms"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "swift-algorithms",
-            dependencies: []),
+            name: "SwiftAlgorithms",
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
-            name: "swift-algorithmsTests",
-            dependencies: ["swift-algorithms"]),
+            name: "SwiftAlgorithmsTests",
+            dependencies: ["SwiftAlgorithms"],
+            path: "Tests"),
     ]
 )
